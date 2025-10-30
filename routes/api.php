1f6bbfe42f963d3c41f66e903e5458aca8727319
Romulo Sousa <romulosousa27@hotmail.com>
2025-10-30 18:44:27 -0300

    chore: adicionando rotas para gerenciar os colaboradores

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CollaboratorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('collaborators', CollaboratorController::class);
+    Route::post('/collaborators/import', [CollaboratorController::class, 'importByFile']);
+});
